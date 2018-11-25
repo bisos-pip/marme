f42c24ae59231e7d9202be36d595d867b5f4de09
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2018-11-24 19:48:12 -0800

    Made example.com part of the distribution.

---
+++ dev/unisos/marme-fbxo/exMarme/so/r3/sr/marme/dsnProc/adapt/admin/adaptMarmeManage.py
@@ -0,0 +1,12 @@
+#
+# IMPORTANT
+#  
+# invokes pkgMarmeManage
+#
+# Place holder for now
+#
+# -i pkgLink would do a
+# ln -s /bisos/vcAuth/git/bxRepos/auth/bisos-pip/marme/dev/unisos/marme-base/admin pkg-admin
+# The path can be obtained from pkgMarmeManage
+#
+
